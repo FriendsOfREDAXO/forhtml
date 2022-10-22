ff65041bd6b58c431eed455767aea12b1171a194
Thomas Skerbis <thomas.skerbis@mac.com>
2022-10-22 22:08:51 +0200

    Create section.php

---
+++ fragments/uk3/section.php
@@ -0,0 +1,33 @@
+<?php
+if (isset($this->help) && $this->help === true) {
+    $help = [];
+    $help['info']         = 'Das Fragment ezeugt UiKit-Cards: https://getuikit.com/assets/uikit/tests/card.html';
+    $help['body']         = 'Body (String)';
+    $help['body_prepend'] = 'vor Body (String)';
+    $help['body_append']  = 'nach Body (String)';
+    $help['attributes']   = 'Hier können Attribute zur uk-card ergänzt werden (array), bei class werden diese an .uk-section angehägnt ';
+    dump($help);
+}
+
+// default class uk-section
+$attributes = [];
+$attributes['class'] = 'uk-section';
+if (isset($this->attributes) && is_array($this->attributes)) {
+    if (array_key_exists('class', $this->attributes)) {
+        $class = $this->attributes['class'];
+        $attributes['class'] = 'uk-card ' . $class;
+    }
+}
+$attributes_out = rex_string::buildAttributes($attributes);
+?>
+<section<?= $attributes_code ?>>
+<?php if (isset($this->body_prepend) && $this->body_prepend != '') : ?>
+<?= $this->body_prepend ?>
+<?php endif; ?>
+<?php if (isset($this->body) && $this->body != '') : ?>
+<?= $this->body ?>
+<?php endif; ?>
+<?php if (isset($this->body_append) && $this->body_append != '') : ?>
+<?= $this->body_append ?>
+<?php endif; ?>
+</section>
