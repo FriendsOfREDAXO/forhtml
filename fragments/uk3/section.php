3cf4c4f8553dd60a80d2714a86e5544fb9693932
Thomas Skerbis <thomas.skerbis@mac.com>
2022-10-22 22:15:04 +0200

    Update section.php

--- fragments/uk3/section.php
+++ fragments/uk3/section.php
@@ -3,8 +3,6 @@ if (isset($this->help) && $this->help === true) {
     $help = [];
     $help['info']         = 'Das Fragment ezeugt eine Section: https://getuikit.com/assets/uikit/tests/section.html';
     $help['body']         = 'Body (String)';
-    $help['body_prepend'] = 'vor Body (String)';
-    $help['body_append']  = 'nach Body (String)';
     $help['attributes']   = 'Hier können Attribute zur ergänzt werden (array), bei class werden diese an .uk-section angehägnt ';
     dump($help);
 }
@@ -20,14 +18,8 @@ if (isset($this->attributes) && is_array($this->attributes)) {
 }
 $attributes_out = rex_string::buildAttributes($attributes);
 ?>
-<section<?= $attributes_code ?>>
-<?php if (isset($this->body_prepend) && $this->body_prepend != '') : ?>
-<?= $this->body_prepend ?>
-<?php endif; ?>
+<section<?= $attributes_out?>>
 <?php if (isset($this->body) && $this->body != '') : ?>
 <?= $this->body ?>
-<?php endif; ?>
-<?php if (isset($this->body_append) && $this->body_append != '') : ?>
-<?= $this->body_append ?>
 <?php endif; ?>
 </section>
