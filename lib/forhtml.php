f9fb085e2d9d589c9f6dfafd8200d1764e4a18e0
Thomas Skerbis <thomas.skerbis@mac.com>
2022-10-24 08:15:23 +0200

    Update forhtml.php

--- lib/forhtml.php
+++ lib/forhtml.php
@@ -3,7 +3,7 @@ use HtmlGenerator\HtmlTag;
 
 class FORHtml extends HtmlTag
 {
-   public function mmfile($type = 'default', $file ='')
+   public function mmfile($type = 'default', $file =''):string
     {
      return $this->set('src', rex_media_manager::getUrl($type, $file));
     } 
