497a9c98b88738fa529789728e71af2bf81067da
Thomas Skerbis <thomas.skerbis@mac.com>
2022-10-27 16:52:17 +0200

    Update forhtml.php

--- lib/forhtml.php
+++ lib/forhtml.php
@@ -5,12 +5,15 @@ class FORHtml extends HtmlTag
 {
    /** @api */
    public function mmfile(string $type = 'default', string $file =''):string
-    {
+   {
      return $this->set('src', rex_media_manager::getUrl($type, $file));
-    } 
-   
-      public function body(string $body =''):string
-    {
+   } 
+
+   public function body(string $body =''):string
+   {
      return $this->text($body);
-    } 
+   }
+   
+   
+   
 }
